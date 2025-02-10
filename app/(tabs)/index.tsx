25c5b672b7fa099384b105ba35d6c662a6b905b6
Joseph Balogun <balogunsunday91@gmail.com>
2025-02-10 12:35:20 +0100

    fix: color plalet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Login } from '../auth';
+import SplashScreen from '../auth/SplashScreen';
 
 export default function HomeScreen() {
   return (
-      <Login />
+      <SplashScreen />
   );
 }
 
