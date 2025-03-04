2c5ebc0932e024410f1f1a5997f7fd71de0ea6f1
Joseph Balogun <balogunsunday91@gmail.com>
2025-03-04 05:04:20 +0100

    fix: OnBoarding Logic Seperation

---
+++ app/auth/OnBoarding/useOnBoardingLogic.tsx
@@ -0,0 +1,33 @@
+import { slides } from "@/constants/Slides";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
+
+const useOnBoardingLogic = () => {
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const flatListRef = useRef<FlatList<any> | null>(null);
+  
+    const onViewableItemsChanged = ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      if (viewableItems.length > 0) {
+        const index = viewableItems[0].index ?? 0;
+        setCurrentIndex(index);
+  
+        if (index === slides.length) {
+          handleSkip();
+        }
+      }
+    };
+    const handleSkip = () => {
+      navigation.navigate("Login");
+    };
+    return {
+        handleSkip,
+        setCurrentIndex,
+        currentIndex,
+        flatListRef,
+        onViewableItemsChanged,
+    }
+}
+
+export default useOnBoardingLogic;
---
+++ constants/Slides.ts
@@ -0,0 +1,8 @@
+import { colors } from ".";
+
+export const slides = [
+    { id: "1", title: "Explore hands-on learning with fun activities!", color: colors.yellow500 },
+    { id: "2", title: "Pick what you love to learn and go at your own pace!", color: colors.blue500 },
+    { id: "3", title: "Complete challenges, earn badges, and grow with every step!", color: colors.red500 },
+    { id: "4", title: "Parents track progress, set goals, and support learning!", color: colors.purple500 },
+  ];
