f8cd2817f2fb916475030ec6ffb9c9f13461a78a
Joseph Balogun <balogunsunday91@gmail.com>
2025-03-20 05:17:52 +0100

    fix: padding

--- components/secondary/GreetingCard/index.tsx
+++ components/secondary/GreetingCard/index.tsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    gap: 30,
+    gap: 15,
     backgroundColor: colors.green500,
     padding: 20,
     borderRadius: 12,
